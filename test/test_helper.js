95700382b4fccd29976cb49cf2d083be5b67b111
Imee Cuison <imeetwelve@gmail.com>
2017-08-21 10:51:32 -0400

    test helper explanation

--- test/test_helper.js
+++ test/test_helper.js
@@ -9,12 +9,19 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
+//set up testing environment to run like browser inthe command line
+
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = _$(window);  // jquery be responsible for fake instance of dom we made (window)
+
+
+// set up chai-jquery
 
 chaiJquery(chai, chai.util, $);
+
+// build 'renderComponent' helper that should render a given react class
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
   const componentInstance =  TestUtils.renderIntoDocument(
@@ -23,8 +30,11 @@ function renderComponent(ComponentClass, props = {}, state = {}) {
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
 }
+
+// build 'renderComponent' helper that should render a given react class
+//.fn opens simulate to all jquery
 
 $.fn.simulate = function(eventName, value) {
   if (value) {
